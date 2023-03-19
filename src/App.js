0d226eecc2440b992cefe3fec72ceacaca500411
FlavioDeAngeli <flaviodeangeli@gmail.com>
2023-03-19 14:29:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { depositMoney, withdrawMoney } from "./store/actions";
 
 function App() {
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{account}</div>
+      <div>
+        <button onClick={() => dispatch(depositMoney(100))}>+</button>
+        <button onClick={() => dispatch(withdrawMoney(100))}>-</button>
+      </div>
     </div>
   );
 }
